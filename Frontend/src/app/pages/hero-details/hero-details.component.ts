9e66f86fd5e513e0e31676778a776d987238406a
Radu Ionut Oancea <roancea22@gmail.com>
2025-05-10 17:01:39 +0200

    Update: added the last details on the hero-details-page and made the pages responsive

--- Frontend/src/app/pages/hero-details/hero-details.component.ts
+++ Frontend/src/app/pages/hero-details/hero-details.component.ts
@@ -12,6 +12,21 @@ import { CommonModule } from '@angular/common';
 })
 export class HeroDetailsComponent implements OnInit {
 
+  capitalizeKey(arg0: unknown): string {
+    if (typeof arg0 === 'string') {
+      return arg0.charAt(0).toUpperCase() + arg0.slice(1);
+    }
+    throw new Error('Invalid argument: arg0 must be a string');
+  }
+
+  getAppearanceValue(value: any): string {
+    if (Array.isArray(value)) {
+      return value[1] ?? 'N/A';
+    }
+    return value && value !== 'null' ? value : 'N/A';
+  }
+
+
   hero: any = null;
 
   constructor(private apiService: ApiFetchService, private route: ActivatedRoute) { }
